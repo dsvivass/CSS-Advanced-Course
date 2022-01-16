f9f2a222b400744bd58e779b06984d3c6df848eb
dsvivass <dsvivass@unal.edu.co>
2022-01-16 11:01:28 -0500

    Selectors

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const f = () => {
+    console.log('Hello world');
+}
+
+const f2 = () => {
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+    console.log('Hello world');
+}
